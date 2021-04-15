0dbda83277fc1c703903c4d8bdb1e80447165ae9
Martin Rossil <martinrossil@gmail.com>
2021-04-15 10:48:15 +0200

    Added TopBar.

--- src/FjedreApp.ts
+++ src/FjedreApp.ts
@@ -1,9 +1,17 @@
-import { ApplicationElement } from 'enta';
+import { AnchorLayout, ApplicationElement, Color, TopBar } from 'enta';
 
 export default class FjedreApp extends ApplicationElement {
     public constructor() {
         super();
-        console.log('FjedreApp constructor()');
+        this.name = 'EntaDev';
+        this.backgroundColor = new Color(210, 40, 96); // blue gray 100
+        this.layout = new AnchorLayout();
+        window.addEventListener('load', () => {
+            console.log('page loaded');
+            this.addElement(new TopBar());
+        });
     }
 }
 customElements.define('fjedre-app', FjedreApp);
+
+// // "value": "cache-control: public,max-age=31536000,immutable"
